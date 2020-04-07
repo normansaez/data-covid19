8312bf580ce9f5e5c0cd5b95da24d4b837ba3c8f
Norman Saez <nsaez@dlab.cl>
2020-04-07 19:48:25 -0300

    json2csv

---
+++ json2csv/app.py
@@ -0,0 +1,27 @@
+import logging
+import time
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/health_check', methods=['GET'])
+def empty_view():
+    app.logger.info('health check')
+    response = {'status': 'OK'}
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+else:
+    # setup logging using gunicorn logger
+    formatter = logging.Formatter(
+        '[%(asctime)s.%(msecs)03d] [%(name)s] [%(levelname)s] - %(message)s',
+        '%d-%m-%Y %H:%M:%S'
+    )
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.handlers[0].setFormatter(formatter)
+    app.logger.setLevel(logging.DEBUG)
