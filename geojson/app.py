dd7b97955d2b981114ed491da15e62cc8aa8af06
Norman Saez <nsaez@dlab.cl>
2020-04-13 09:46:52 -0400

    reordering code

--- geojson/app.py
+++ geojson/app.py
@@ -18,30 +18,30 @@ mongo = PyMongo(app)
 client = pymongo.MongoClient("mongodb://192.168.2.223:27017")
 
 
-def dump_func_name(func):
-    def echo_func(*func_args, **func_kwargs):
-        app.logger.info('EXEC: {}'.format(func.__name__))
-        return func(*func_args, **func_kwargs)
-    return echo_func
-
-#@dump_func_name
 @app.route('/health_check', methods=['GET'])
-def empty_view():
+def health_check():
+    '''
+    health_check
+    '''
     app.logger.info("health_check")
     response = {'status': 'OK'}
     return jsonify(response), 200
 
-#@dump_func_name
-@app.route('/get_comunas', methods=['GET'])
+@app.route('/v1/get_comunas', methods=['GET'])
 def get_comunas():
+    '''
+    get_comunas
+    '''
     app.logger.info("get_comunas")
     comunas = mongo.db.list_collection_names()
     response = {'comunas':comunas} 
     return jsonify(response), 200
 
-#@dump_func_name
-@app.route('/get_comuna_by_name', methods=['GET'])
+@app.route('/v1/get_comuna_by_name', methods=['GET'])
 def get_comuna_by_name():
+    '''
+    get_comuna_by_name
+    '''
     app.logger.info("get_comuna_by_name")
     try:
         comuna_id = request.args.get('comuna')
@@ -58,6 +58,48 @@ def get_comuna_by_name():
         doc.pop("_id",None)
         response = doc
     return jsonify(response), 200
+
+@app.route('/v1/get_comuna_by_cut', methods=['GET'])
+def get_comuna_by_cut():
+    '''
+    get_comuna_by_cut:
+    - Te paso el código de comuna y el parámetro de simplificación
+    - Me retornas el topojson de la comuna correspondiente
+    '''
+    app.logger.info("get_comuna_by_cut")
+    comuna_id = request.args.get('comuna')
+    app.logger.info("Comuna ID: {}".format(comuna_id))
+    return jsonify(response), 200
+
+@app.route('/v1/get_region_by_id', methods=['GET'])
+def get_region_by_id():
+    '''
+    get_region_by_id: 
+    - Te paso el código de región y el parámetro de simplificación
+    - Me retornas el topojson de la región correspondiente
+    
+    (Este no lo tengo tan definido en estructura ni nombre, pero nos puede ahorrar
+    tiempo evitando hacer tantos requests por cada región. Siempre vamos a pedir
+    todos los polígonos de las comunas de la región a visualizar)
+    '''
+    app.logger.info("get_region_by_id")
+    comuna_id = request.args.get('comuna')
+    app.logger.info("Comuna ID: {}".format(comuna_id))
+    return jsonify(response), 200
+
+@app.route('/v1/get_comunas_by_region_id', methods=['GET'])
+def get_comunas_by_region_id():
+    '''
+    get_comunas_by_region_id:
+    - Te paso el código de región y el parámetro de simplificación
+    - Me retornas el topojson con los polígonos de todas las comunas de la región correspondiente
+    
+    '''
+    app.logger.info("get_comunas_by_region_id")
+    comuna_id = request.args.get('comuna')
+    app.logger.info("Comuna ID: {}".format(comuna_id))
+    return jsonify(response), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ geojson/utils/load_files.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+# -- coding: utf-8 --
+import glob
+from pathlib import Path
+import os
+import json
+import pymongo
+import gridfs
+
+if __name__ == '__main__':
+    client = pymongo.MongoClient()
+    db = client["comunas"]
+    fs = gridfs.GridFS(client.comunas)
+
+    files = []
+    path = Path('.').rglob('*.json')
+    #    path.rename(path.name.replace(' ','_'))
+    for i in path:
+       files.append(i.name)
+    print("total files: {}".format(len(files))  ) 
+    files.sort(key=lambda f: os.stat(f).st_size, reverse=True)
+    for name in files:
+        print(name)
+        with open(name, 'r') as jsonfile:
+            json_data = json.load(jsonfile)
+        #print(json_data)
+        
+        col = db[name.rstrip('.json')]
+        try:
+            x = col.insert_one(json_data)
+        #    print(x)
+        except pymongo.errors.DocumentTooLarge:
+            print("{}\n could not be inserted".format(name))
+            with open(name, 'rb') as jsonfile:
+                a = fs.put(jsonfile)
+            dict_name_pointer = {name:a}
+            print(dict_name_pointer)
+#    x = col.find_one()
+#    print(db.getCollectionNames())
+        #    x = col.find_one()
