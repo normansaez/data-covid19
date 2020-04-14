429ffbe9fda8522d371a8efc98533448f2761863
Norman Saez <nsaez@dlab.cl>
2020-04-14 09:35:56 -0400

    updating endpoints

--- geojson/utils/check_endpoints.bash
+++ geojson/utils/check_endpoints.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 curl http://192.168.2.223:5000/health_check
 curl http://192.168.2.223:5000/v1/get_comunas
-#curl http://192.168.2.223:5000/v1/get_comuna_by_name?comuna=13130_SAN_MIGUEL
-curl http://192.168.2.223:5000/v1/get_comuna_by_name?comuna=13130
-curl http://192.168.2.223:5000/v1/get_comuna_by_cut?comuna=13130&simpl_number=50
-curl http://192.168.2.223:5000/v1/get_region_by_id
-curl http://192.168.2.223:5000/v1/get_comunas_by_region_id
+curl http://192.168.2.223:5000/v1/get_regiones
+curl http://192.168.2.223:5000/v1/get_comuna_by_region_id?region=5&simplify=0.11
+curl http://192.168.2.223:5000/v1/get_region_by_id?region=5&simplify=0.11
