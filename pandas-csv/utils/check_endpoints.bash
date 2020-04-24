94a9bb391fec402780420badc5e87f4fac45fb9d
Norman Saez <nsaez@dlab.cl>
2020-04-24 01:55:23 -0400

    updating checkpoint

--- pandas-csv/utils/check_endpoints.bash
+++ pandas-csv/utils/check_endpoints.bash
@@ -1,6 +1,2 @@
 #!/bin/bash
-curl http://192.168.2.223:5000/health_check
-curl http://192.168.2.223:5000/v1/get_comunas
-curl http://192.168.2.223:5000/v1/get_regiones
-curl http://192.168.2.223:5000/v1/get_comuna_by_region_id?region=5&simplify=0.11
-curl http://192.168.2.223:5000/v1/get_region_by_id?region=5&simplify=0.11
+curl http://192.168.2.223:5004/v1/get_movilidad?month=1&year=2019
