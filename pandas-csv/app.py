cf3a8d2578dfc9327257710775e3c73b415431c3
Norman Saez <nsaez@dlab.cl>
2020-04-25 14:21:16 -0400

    methods

--- pandas-csv/app.py
+++ pandas-csv/app.py
@@ -45,6 +45,31 @@ def get_movilidad():
     response = doc
     return jsonify(response), 200
 
+@app.route('/v2/get_movilidad', methods=['GET'])
+@app.route('/v2/get_movilidad_by_cut', methods=['GET'])
+def get_movilidad_by_cut():
+    '''
+    get_movilidad
+    '''
+    app.logger.info("v2: get_movilidad")
+    monthinteger = int(request.args.get('month'))
+    year = request.args.get('year')
+    month = datetime.date(1900, monthinteger, 1).strftime('%b').upper()
+
+    app.logger.info("month: {}".format(month))
+    app.logger.info("year: {}".format(year))
+
+    movi = "{}-{}".format(year,month)
+
+    db = client['movilidad_by_cut']
+    collec = db[movi]
+    doc = collec.find_one()
+    if doc == None:
+        return jsonify({"status":"not found"}), 200
+    doc.pop("_id",None)
+    response = doc
+    return jsonify(response), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5004, debug=True, ssl_context=('cert.pem', 'key.pem'))
