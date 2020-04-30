daf3009ff72c9256b7b64f0d976faa065a3c66b8
Norman Saez <normansaez.globant@latam.com>
2020-04-30 01:51:16 -0400

    last stats

--- tests/hit_endpoints.py
+++ tests/hit_endpoints.py
@@ -10,9 +10,29 @@ def read_enpoints(filename="endpoints.yaml"):
 
 if __name__ == '__main__':
     endpoints = read_enpoints()
-    print(endpoints)
+    notjason = 0
+    err = 0
+    l_notjson= []
+    l_err = []
     for endpoint in endpoints:
         print(endpoint)
         r = requests.get(endpoint) 
-        df = pd.DataFrame.from_dict(r.json())
-        print(df)
+        try:
+            df = pd.DataFrame.from_dict(r.json())
+            print(df)
+        except json.decoder.JSONDecodeError:
+            l_notjson.append(endpoint)
+            notjason += 1
+        except:
+            l_err.append(endpoint)
+            err += 1
+#####################################################################
+total = len(endpoint)
+jres =  len(endpoint) - notjason - err
+print("Stats\n")
+for i in l_notjson:
+    print("not json: {}".format(i))
+for i in l_err:
+    print("error: {}".format(i))
+print("\nTotal endpoints: {} , json response {} , not json {}, error {}".format(total, jres, notjason, err))
+
