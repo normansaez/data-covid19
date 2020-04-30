3025a3eae1a506c751e9f09448e95b6bda543647
Norman Saez <normansaez.globant@latam.com>
2020-04-29 21:34:25 -0400

    restore movilidad_por_comuna

--- pandas-csv/app.py
+++ pandas-csv/app.py
@@ -71,8 +71,29 @@ def get_movilidad_by_cut():
     response = doc
     return jsonify(response), 200
 
-#activos_comunas_dia_abril
-#activos_comunas_fecha_abril
+@app.route('/v1/get_movilidad_por_comuna', methods=['GET'])
+def get_movilidad_por_comuna():
+    '''
+    get_movilidad
+    '''
+    app.logger.info("v2: get_movilidad_por_comuna")
+#    monthinteger = int(request.args.get('month'))
+#    year = request.args.get('year')
+#    month = datetime.date(1900, monthinteger, 1).strftime('%b').upper()
+
+#    app.logger.info("month: {}".format(month))
+#    app.logger.info("year: {}".format(year))
+
+    movi = "diaria-JAN"#"{}-{}".format(year,month)
+
+    db = client['movilidad_por_comuna']
+    collec = db[movi]
+    doc = collec.find_one()
+    if doc == None:
+        return jsonify({"status":"not found"}), 200
+    doc.pop("_id",None)
+    response = doc
+    return jsonify(response), 200
 
 @app.route('/v1/activos_comunas_por_dia_contagio', methods=['GET'])
 def get_activos_comunas_dia_abril():
