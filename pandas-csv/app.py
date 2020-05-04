aef9ca84ab17a006245038ec2c9be2ed0352c763
Norman Saez <normansaez.globant@latam.com>
2020-05-04 16:56:30 -0400

    sochimi

--- pandas-csv/app.py
+++ pandas-csv/app.py
@@ -71,6 +71,33 @@ def get_movilidad_by_cut():
     response = doc
     return jsonify(response), 200
 
+@app.route('/v2/get_sochimi', methods=['GET'])
+def get_sochimi():
+    '''
+    get_movilidad
+    '''
+    app.logger.info("v2: get_sochimi")
+    monthinteger = int(request.args.get('month'))
+    year = request.args.get('year')
+    day = request.args.get('day')
+    month = datetime.date(1900, monthinteger, 1).strftime('%b').upper()
+
+    app.logger.info("day: {}".format(day))
+    app.logger.info("month: {}".format(month))
+    app.logger.info("year: {}".format(year))
+
+    movi = "{}_{}_{}_UPC".format(year,month,day)
+    #2020_APR_14_UPC
+
+    db = client['SOCHIMIUPS']
+    collec = db[movi]
+    doc = collec.find_one()
+    if doc == None:
+        return jsonify({"status":"not found"}), 200
+    doc.pop("_id",None)
+    response = doc
+    return jsonify(response), 200
+
 @app.route('/v1/get_movilidad_por_comuna', methods=['GET'])
 def get_movilidad_por_comuna():
     '''
--- pandas-csv/utils/load_files.py
+++ pandas-csv/utils/load_files.py
@@ -10,19 +10,22 @@ import pandas as pd
 
 if __name__ == '__main__':
     client = pymongo.MongoClient("mongodb://192.168.2.223:27017")
-    nombre_bd = 'activos_comunas'
-    collection = "activos_comunas_dia_marzo"
+    extension = 'xlsx' 
+    nombre_bd = 'SOCHIMIUPS'
+#    collection = "activos_comunas_dia_marzo"
     db = client[nombre_bd]
-    path = Path('.').rglob('*.csv')
+    path = Path('.').rglob('*.{}'.format(extension))
     for filename in path:
 #        print(filename.name)
-#        collection = filename.name.rsplit('.csv')[0]
+        collection = filename.name.rsplit('.{}'.format(extension))[0]
         print(collection)
-        read_file = pd.read_csv(filename.name, encoding= 'unicode_escape')
+#        read_file = pd.read_csv(filename.name, encoding= 'unicode_escape')
+        read_file = pd.read_excel(filename.name, encoding= 'unicode_escape')
         json_data = json.loads(read_file.to_json(orient="index"))
 
         col = db[collection]
         try:
             x = col.insert_one(json_data)
-        except pymongo.errors.DocumentTooLarge:
-            print("{}\n could not be inserted".format(name))
+        except Exception as ex:
+            print("{}\n could not be inserted: {}".format(collection,str(ex)))
+            
